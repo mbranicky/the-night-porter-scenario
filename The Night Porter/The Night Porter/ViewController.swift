6bb176c91340d86d697907aa216e4f1b3f3a950e
mbranicky <marek.branicky@gmail.com>
2019-07-17 18:05:49 +0200

    Initial commit. First static list of random tasks tested.

---
+++ The Night Porter/The Night Porter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Night Porter
+//
+//  Created by marek on 15/07/2019.
+//  Copyright © 2019 Webby, s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
